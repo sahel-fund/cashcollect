c0a6223321d80ffa3776c8867698fa7794c99504
baimamboukar <baimamboukar@gmail.com>
2022-09-21 17:00:49 +0100

    CashCollect

---
+++ lib/src/config/palette.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/rendering.dart';
+
+class Palette {
+  static const Color primary = Color(0xFF8A80B5);
+  static const Color secondary = Color(0xFFC886BA);
+  static const Color tertiary = Color(0xFFFD8FAA);
+  static const Color quaternary = Color(0xFFFFA68B);
+  static const Color quinary = Color(0xFFFFCC70);
+  static const Color senary = Color(0xFFF9F871);
+
+  // GENERIC PALETTE
+  static const Color dark = Color(0xFF000000);
+  static const Color light = Color(0xFFFFFFFF);
+  static const Color grey = Color(0xFFE0E0E0);
+  static const Color darkGrey = Color(0xFF9E9E9E);
+  static const Color lightGrey = Color(0xFFF5F5F5);
+  static const Color danger = Color(0xFFE53935);
+  static const Color success = Color(0xFF43A047);
+  static const Color info = Color(0xFF1E88E5);
+  static const Color yellow = Color(0xFFFFC107);
+  static const Color orange = Color(0xFFFF9800);
+}
