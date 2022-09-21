1f7c3159582c6ab22f76e7eb884045af57d9d702
baimamboukar <baimamboukar@gmail.com>
2022-09-21 18:17:03 +0100

    shared scaffold done

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -1,4 +1,6 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:cashcollect/src/screens/auth/login.dart';
+import 'package:cashcollect/src/screens/auth/signup.dart';
 import 'package:cashcollect/src/screens/home.dart';
 import 'package:cashcollect/src/screens/welcome.dart';
 
@@ -7,6 +9,8 @@ import 'package:cashcollect/src/screens/welcome.dart';
   routes: <AutoRoute>[
     AutoRoute(page: Welcome, path: '/', initial: true),
     AutoRoute(page: Home, path: '/home'),
+    AutoRoute(page: Login, path: '/login'),
+    AutoRoute(page: Signup, path: '/signup'),
   ],
 )
 class $AppRouter {}
