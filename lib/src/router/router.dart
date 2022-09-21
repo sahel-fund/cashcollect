11de007100763254e531d4583c0fa271dabcc954
baimamboukar <baimamboukar@gmail.com>
2022-09-21 19:24:06 +0100

    dynamic theming

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cashcollect/src/screens/auth/login.dart';
 import 'package:cashcollect/src/screens/auth/signup.dart';
+import 'package:cashcollect/src/screens/history.dart';
 import 'package:cashcollect/src/screens/home.dart';
+import 'package:cashcollect/src/screens/intro.dart';
 import 'package:cashcollect/src/screens/settings.dart';
 import 'package:cashcollect/src/screens/survey/surveys.dart';
 import 'package:cashcollect/src/screens/welcome.dart';
@@ -11,9 +13,9 @@ import 'package:cashcollect/src/screens/welcome.dart';
   routes: <AutoRoute>[
     AutoRoute(page: Welcome, path: '/', initial: true),
     AutoRoute(page: Home, path: '/home', children: [
-      AutoRoute(page: Login, path: 'login'),
-      AutoRoute(page: Signup, path: 'signup'),
+      AutoRoute(page: Intro, path: 'intro'),
       AutoRoute(page: Surveys, path: 'surveys'),
+      AutoRoute(page: History, path: 'history'),
       AutoRoute(page: Settings, path: 'settings'),
     ]),
   ],
