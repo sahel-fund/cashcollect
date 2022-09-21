48721a426fec5c8ff5086e7bb194dbd66fbbfbe3
baimamboukar <baimamboukar@gmail.com>
2022-09-21 18:48:11 +0100

    shared scaffold screens

---
+++ lib/src/screens/settings.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Settings extends ConsumerWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      child: const Center(
+        child: Text(
+          'Settings',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
