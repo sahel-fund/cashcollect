7bcdcefe8fa83f290611748ef2d4e8200b38f5db
baimamboukar <baimamboukar@gmail.com>
2022-09-21 18:03:47 +0100

    refactored to auto_route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:cashcollect/src/config/palette.dart';
-import 'package:cashcollect/src/router/router.dart';
+import 'package:cashcollect/src/router/router.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -7,21 +7,21 @@ import 'package:google_fonts/google_fonts.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    const ProviderScope(
+    ProviderScope(
       child: CashCollect(),
     ),
   );
 }
 
 class CashCollect extends ConsumerWidget {
-  const CashCollect({Key? key}) : super(key: key);
-
+  CashCollect({Key? key}) : super(key: key);
+  final _appRouter = AppRouter();
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
       restorationScopeId: 'cashcollect',
       title: 'CashCollect',
-      routerConfig: router,
+      routeInformationParser: _appRouter.defaultRouteParser(),
       color: Palette.primary,
       theme: ThemeData(
         primaryColor: Palette.primary,
