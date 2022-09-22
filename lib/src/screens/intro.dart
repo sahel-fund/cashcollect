fc34eb4c739a78128d6e8f69d7bd1edee4e454e6
baimamboukar <baimamboukar@gmail.com>
2022-09-22 17:42:15 +0100

    home boxes done

--- lib/src/screens/intro.dart
+++ lib/src/screens/intro.dart
@@ -1,5 +1,7 @@
 import 'package:cashcollect/src/config/palette.dart';
 import 'package:cashcollect/src/config/text_styles.dart';
+import 'package:cashcollect/src/widgets/box.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -10,13 +12,54 @@ class Intro extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Center(
-        child: Text(
-          'Intro',
-          style: TextStyles.designText(
-            bold: true,
-            size: 22,
-            color: Palette.secondary,
-          ),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 220,
+            ),
+            Wrap(
+              spacing: 20,
+              runSpacing: 20,
+              children: const [
+                Box(
+                  title: 'SURVEYS',
+                  icon: Icon(
+                    EvaIcons.barChart,
+                    size: 54,
+                    color: Palette.lightGrey,
+                  ),
+                  desc: 'Complete surveys',
+                ),
+                Box(
+                  title: 'HISTORY',
+                  icon: Icon(
+                    Icons.newspaper,
+                    size: 54,
+                    color: Palette.lightGrey,
+                  ),
+                  desc: 'Review answered surveys',
+                ),
+                Box(
+                  title: 'WITHDRAW',
+                  icon: Icon(
+                    Icons.currency_exchange,
+                    size: 54,
+                    color: Palette.lightGrey,
+                  ),
+                  desc: 'Withdraw cash',
+                ),
+                Box(
+                  title: 'SUPPORT',
+                  icon: Icon(
+                    Icons.support_agent,
+                    size: 54,
+                    color: Palette.lightGrey,
+                  ),
+                  desc: 'Ask for help',
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
