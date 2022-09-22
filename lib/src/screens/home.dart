18312db5f701a6645f963b7fcce5b0bdea4bb6a9
baimamboukar <baimamboukar@gmail.com>
2022-09-22 16:27:58 +0100

    designing logo widget

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -4,66 +4,74 @@ import 'package:cashcollect/src/router/router.gr.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class Home extends ConsumerWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return AutoTabsScaffold(
-      routes: const [
-        Intro(),
-        Surveys(),
-        History(),
-        Settings(),
-      ],
-      builder: (context, child, animation) => ScaleTransition(
-        scale: animation,
-        child: child,
+    return SafeArea(
+      child: AutoTabsScaffold(
+        routes: const [
+          Intro(),
+          Surveys(),
+          History(),
+          Settings(),
+        ],
+        builder: (context, child, animation) => ScaleTransition(
+          scale: animation,
+          child: child,
+        ),
+        bottomNavigationBuilder: (context, router) {
+          return ValueListenableBuilder(
+            valueListenable: Hive.box('settings').listenable(),
+            builder: (BuildContext context, Box box, Widget? widget) =>
+                BottomNavigationBar(
+              elevation: 12,
+              backgroundColor:
+                  box.get('theme') ? Palette.backgroundDark : Palette.lightGrey,
+              selectedItemColor: Palette.primary,
+              unselectedItemColor: Palette.primary.withOpacity(.55),
+              currentIndex: router.activeIndex,
+              onTap: (index) => router.setActiveIndex(index),
+              type: BottomNavigationBarType.fixed,
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.dashboard,
+                    size: 18,
+                  ),
+                  label: "Home",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    EvaIcons.barChart,
+                    size: 18,
+                  ),
+                  label: "Surveys",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.newspaper,
+                    size: 18,
+                  ),
+                  backgroundColor: Palette.dark,
+                  label: "Historique",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    EvaIcons.settings,
+                    size: 18,
+                  ),
+                  backgroundColor: Palette.dark,
+                  label: "Settings",
+                ),
+              ],
+            ),
+          );
+        },
       ),
-      bottomNavigationBuilder: (context, router) {
-        return BottomNavigationBar(
-          elevation: 12,
-          // backgroundColor: Colors.white,
-          selectedItemColor: Palette.primary,
-          unselectedItemColor: Palette.primary.withOpacity(.55),
-          currentIndex: router.activeIndex,
-          onTap: (index) => router.setActiveIndex(index),
-          type: BottomNavigationBarType.fixed,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.dashboard,
-                size: 18,
-              ),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                EvaIcons.barChart,
-                size: 18,
-              ),
-              label: "Surveys",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.newspaper,
-                size: 18,
-              ),
-              backgroundColor: Palette.dark,
-              label: "Historique",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                EvaIcons.settings,
-                size: 18,
-              ),
-              backgroundColor: Palette.dark,
-              label: "Settings",
-            ),
-          ],
-        );
-      },
     );
   }
 }
