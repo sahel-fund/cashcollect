9b8185de67d7202ae17676b1a1e1e6606b0abb53
baimamboukar <baimamboukar@gmail.com>
2022-09-22 16:27:58 +0100

    designing logo widget

---
+++ lib/src/widgets/mark.dart
@@ -0,0 +1,46 @@
+import 'package:cashcollect/src/config/palette.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TradeMark extends StatelessWidget {
+  const TradeMark({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Cash',
+                style: TextStyle(
+                  color: Palette.primary,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Collect',
+                style: GoogleFonts.aclonica(
+                  color: Palette.tertiary,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+          const Text(
+            'v1.0.0',
+            style: TextStyle(
+              color: Colors.black38,
+              fontSize: 12,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
