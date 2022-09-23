3a70451df70a2581a21dc216234bc9cc8e91cf91
baimamboukar <baimamboukar@gmail.com>
2022-09-23 15:48:33 +0100

    working on signup

--- lib/src/screens/auth/signup.dart
+++ lib/src/screens/auth/signup.dart
@@ -1,5 +1,7 @@
 import 'package:cashcollect/src/config/palette.dart';
 import 'package:cashcollect/src/config/text_styles.dart';
+import 'package:cashcollect/src/widgets/input.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,14 +11,53 @@ class Signup extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Signup',
-          style: TextStyles.designText(
-            bold: true,
-            size: 22,
-            color: Palette.secondary,
-          ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 34),
+            Input(
+              icon: EvaIcons.person,
+              label: 'Names',
+              hint: 'Alpha Casher',
+              validator: (value) {
+                return null;
+              },
+              isPassword: false,
+              controller: TextEditingController(),
+            ),
+            Input(
+              icon: EvaIcons.person,
+              label: 'Email',
+              hint: 'alphacasher@cashcollect.com',
+              validator: (value) {
+                return null;
+              },
+              isPassword: false,
+              controller: TextEditingController(),
+            ),
+            Input(
+              icon: EvaIcons.person,
+              label: 'Phone number',
+              type: TextInputType.number,
+              hint: '+237 690535759',
+              validator: (value) {
+                return null;
+              },
+              isPassword: false,
+              controller: TextEditingController(),
+            ),
+            Input(
+              icon: EvaIcons.person,
+              label: 'Town',
+              hint: 'Ngaoundere',
+              validator: (value) {
+                return null;
+              },
+              isPassword: false,
+              controller: TextEditingController(),
+            ),
+          ],
         ),
       ),
     );
