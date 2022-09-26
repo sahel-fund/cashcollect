a327164aa07e08e094a69189ff3dc14b9e174c0f
baimamboukar <baimamboukar@gmail.com>
2022-09-26 16:28:20 +0100

    sending veriff

---
+++ lib/src/riverpods/auth_riverpods.dart
@@ -0,0 +1,13 @@
+import 'package:cashcollect/src/services/firebase/auth/phone_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AuthRiverpods {
+  static final authStateChangesProvider =
+      StreamProvider.autoDispose<User?>((ref) {
+    return FirebaseAuth.instance.authStateChanges();
+  });
+  static final firebaseAuthProvider = Provider((ref) => FirebaseAuth.instance);
+  static final authenticationProvider =
+      Provider((ref) => Authentication(ref.read));
+}
