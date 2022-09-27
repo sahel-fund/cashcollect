3f25291ccd3f0435502c83d4f467879deb96e879
baimamboukar <baimamboukar@gmail.com>
2022-09-27 14:11:50 +0100

    commiting hive adapters

---
+++ lib/src/utils/adapters/user_adapter.dart
@@ -0,0 +1,17 @@
+import 'package:cashcollect/src/models/user_model.dart';
+import 'package:hive_flutter/adapters.dart';
+
+class DateTimeAdapter extends TypeAdapter<UserModel> {
+  @override
+  final typeId = 0;
+
+  @override
+  UserModel read(BinaryReader reader) {
+    return reader.read();
+  }
+
+  @override
+  void write(BinaryWriter writer, UserModel obj) {
+    writer.write(obj);
+  }
+}
