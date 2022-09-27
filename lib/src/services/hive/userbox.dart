8a93525787859c4eef32b438dbd49e141e32c529
baimamboukar <baimamboukar@gmail.com>
2022-09-27 18:47:45 +0100

    hive done

--- lib/src/services/hive/userbox.dart
+++ lib/src/services/hive/userbox.dart
@@ -3,10 +3,16 @@ import 'package:hive_flutter/adapters.dart';
 
 class UserBox {
   static void add(UserModel user) {
+    if (!(Hive.isBoxOpen('userBox'))) {
+      Hive.openBox<UserModel>('userBox');
+    }
     Hive.box('userBox').put(user.uid, user.toJson());
   }
 
   static UserModel? getUser(String uid) {
+    if (!(Hive.isBoxOpen('userBox'))) {
+      Hive.openBox<UserModel>('userBox');
+    }
     final user = Hive.box('userBox').get(uid);
 
     return UserModel.fromJson(user);
