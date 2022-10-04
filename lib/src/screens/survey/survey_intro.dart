fd96b21e68ca5872fe12ce8e3c3a15f5c4e8e415
baimamboukar <baimamboukar@gmail.com>
2022-10-04 14:30:50 +0100

    created questionview

--- lib/src/screens/survey/survey_intro.dart
+++ lib/src/screens/survey/survey_intro.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+class SurveyIntro extends ConsumerWidget {
+  const SurveyIntro({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold();
+  }
+}
