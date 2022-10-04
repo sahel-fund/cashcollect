4faa5ccd820d6a19d43e2ec68eb8b70488e72148
baimamboukar <baimamboukar@gmail.com>
2022-10-04 16:30:01 +0100

    working with fake data

--- lib/src/models/survey_model.dart
+++ lib/src/models/survey_model.dart
@@ -18,3 +18,121 @@ class SurveyModel {
     required this.questions,
   });
 }
+
+final List<SurveyModel> surveys = [
+  SurveyModel(
+    description: "Presidenial Elections",
+    title: "2025 Elections",
+    color: "#000000",
+    image:
+        "https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg?cs=srgb&dl=pexels-pixabay-56866.jpg&fm=jpg",
+    reward: 1000,
+    questions: [
+      SurveyQuestion(
+        question: "Who is the best candidate?",
+        options: [
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate B"),
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate C"),
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate A"),
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate D"),
+        ],
+      ),
+      SurveyQuestion(
+        question: "Who is the best candidate?",
+        options: [
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate B"),
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate C"),
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate A"),
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate D"),
+        ],
+      ),
+      SurveyQuestion(
+        question: "Who is the best candidate?",
+        options: [
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate B"),
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate C"),
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate A"),
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate D"),
+        ],
+      ),
+      SurveyQuestion(
+        question: "Who is the best candidate?",
+        options: [
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate B"),
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate C"),
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate A"),
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate D"),
+        ],
+      ),
+      SurveyQuestion(
+        question: "Who is the best candidate?",
+        options: [
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate B"),
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate C"),
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate A"),
+          Option(
+              image:
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+              option: "Candidate D"),
+        ],
+      ),
+    ],
+  ),
+];
